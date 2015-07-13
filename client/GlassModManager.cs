2d72a0ac449222ac7e1cf547909e99a382d0571a
Jincux <dephurites@gmail.com>
2015-07-13 18:10:48 -0400

    added boards to mod manager, more stable

--- client/GlassModManager.cs
+++ client/GlassModManager.cs
@@ -40,11 +40,13 @@ function GlassModManager::addActivityEvent(%this, %id, %image, %text, %action, %
 }
 
 function GlassModManager::renderActivity() {
+  GlassModManager_ActivityFeed.clear();
   %currentY = 0;
   %lastDate = "";
   for(%i = 0; %i < GlassModManagerActivityList.getCount(); %i++) {
     %event = GlassModManagerActivityList.getObject(%i);
     if(%lastDate !$= getWord(%event.datestring, 0)) {
+      %currentY += 5;
       %title = new GuiTextCtrl() {
          profile = "BlockButtonProfile";
          horizSizing = "right";
@@ -58,7 +60,7 @@ function GlassModManager::renderActivity() {
          text = getWord(%event.datestring, 0);
          maxLength = "255";
       };
-      %currentY += 35;
+      %currentY += 30;
       GlassModManager_ActivityFeed.add(%title);
     }
 
@@ -134,8 +136,201 @@ function GlassModManager::renderActivity() {
 //    }
 //  ]
 
+function GlassModManager::setPane(%pane) {
+  for(%a = 0; %a < 4; %a++) {
+    %obj = "GlassModManager_Pane" @ %a+1;
+    %obj.setVisible(false);
+  }
+
+  %obj = "GlassModManager_Pane" @ %pane;
+  %obj.setVisible(true);
+}
+
+
+//====================================
+// Boards
+//====================================
+
+function GlassModManager::loadBoards() {
+  %url = "http://" @ BLG.address @ "/api/mm.php?request=boards";
+  %method = "GET";
+  %downloadPath = "";
+  %className = "GlassModManagerBoardsTCP";
+
+  %tcp = connectToURL(%url, %method, %downloadPath, %className);
+}
+
+function GlassModManager::addBoard(%this, %id, %image, %title, %fileCount) {
+  if(!isObject(GlassModManagerBoards)) {
+    new ScriptGroup(GlassModManagerBoards);
+  }
+
+  %so = new ScriptObject() {
+    class = GlassModManagerBoard;
+    id = %id;
+    image = %image;
+    title = %title;
+    filecount = %fileCount;
+  };
+
+  GlassModManagerBoards.add(%so);
+}
+
+function GlassModManager::renderBoards() {
+  GlassModManager_Boards.clear();
+  %currentY = 10;
+  for(%i = 0; %i < GlassModManagerBoards.getCount(); %i++) {
+    %bo = GlassModManagerBoards.getObject(%i);
+
+    %board = new GuiSwatchCtrl("GlassModManager_Board_" @ %bo.id) {
+       profile = "GuiDefaultProfile";
+       horizSizing = "center";
+       vertSizing = "bottom";
+       position = 10 SPC %currentY;
+       extent = "475 30";
+       minExtent = "8 2";
+       enabled = "1";
+       visible = "1";
+       clipToParent = "1";
+       color = "172 216 230 255";
+
+       new GuiBitmapCtrl() {
+          profile = "GuiDefaultProfile";
+          horizSizing = "right";
+          vertSizing = "center";
+          position = "7 7";
+          extent = "16 16";
+          minExtent = "8 2";
+          enabled = "1";
+          visible = "1";
+          clipToParent = "1";
+          bitmap = "Add-Ons/System_BlocklandGlass/image/icon/" @ %bo.image @ ".png";
+          wrap = "0";
+          lockAspectRatio = "0";
+          alignLeft = "0";
+          alignTop = "0";
+          overflowImage = "0";
+          keepCached = "0";
+          mColor = "255 255 255 255";
+          mMultiply = "0";
+       };
+       new GuiMLTextCtrl() {
+          profile = "GuiMLTextProfile";
+          horizSizing = "right";
+          vertSizing = "center";
+          position = "30 7";
+          extent = "429 16";
+          minExtent = "8 2";
+          enabled = "1";
+          visible = "1";
+          clipToParent = "1";
+          lineSpacing = "2";
+          allowColorChars = "0";
+          maxChars = "-1";
+          text = "<font:arial bold:16>" @ %bo.title;
+          maxBitmapHeight = "-1";
+          selectable = "1";
+          autoResize = "1";
+       };
+       new GuiMouseEventCtrl("GlassModManager_BoardButton") {
+          boardId = %bo.id;
+          profile = "GuiDefaultProfile";
+          horizSizing = "right";
+          vertSizing = "bottom";
+          position = "0 0";
+          extent = "465 30";
+          minExtent = "8 2";
+          enabled = "1";
+          visible = "1";
+          clipToParent = "1";
+          lockMouse = "0";
+       };
+    };
+    GlassModManager_Boards.add(%board);
+    %currentY += 32;
+  }
+}
+
+function GlassModManager_BoardButton::onMouseEnter(%this) {
+  %board = "GlassModManager_Board_" @ %this.boardId;
+  %board.color = "150 200 255 255";
+}
+
+function GlassModManager_BoardButton::onMouseLeave(%this) {
+  %board = "GlassModManager_Board_" @ %this.boardId;
+  %board.color = "172 216 230 255";
+}
+
+function GlassModManager_BoardButton::onMouseDown(%this) {
+  GlassModManager.loadBoard(%this.boardId);
+}
+
+function GlassModManagerBoardsTCP::onDone(%this) {
+  if(!%error) {
+		%array = parseJSON(%this.buffer);
+    GlassModManagerBoards.deleteAll();
+		if(getJSONType(%array) $= "array") {
+			for(%i = 0; %i < %array.length; %i++) {
+				%obj = %array.item[%i];
+        %id = %obj.get("id");
+        %image = %obj.get("image");
+        %name = %obj.get("name");
+        %files = %obj.get("files");
+        GlassModManager.addBoard(%id, %image, %name, %files);
+			}
+      GlassModManager.renderBoards();
+		} else {
+    }
+	} else {
+  }
+}
+
+function GlassModManagerBoardsTCP::handleText(%this, %line) {
+	%this.buffer = %this.buffer NL %line;
+}
+
+
+//====================================
+// Board Loading
+//====================================
+
+function GlassModManager::loadBoard(%this, %id) {
+  %url = "http://" @ BLG.address @ "/api/mm.php?request=board&board=" @ %id;
+  %method = "GET";
+  %downloadPath = "";
+  %className = "GlassModManagerBoardTCP";
+
+  %tcp = connectToURL(%url, %method, %downloadPath, %className);
+}
+
+function GlassModManagerBoardTCP::onDone(%this) {
+  if(!%error) {
+		%array = parseJSON(%this.buffer);
+    GlassModManagerBoards.deleteAll();
+		if(getJSONType(%array) $= "array") {
+			for(%i = 0; %i < %array.length; %i++) {
+				%obj = %array.item[%i];
+        %id = %obj.get("id");
+        %image = %obj.get("image");
+        %name = %obj.get("name");
+        %files = %obj.get("files");
+        GlassModManager.addBoard(%id, %image, %name, %files);
+			}
+      GlassModManager.renderBoards();
+		} else {
+    }
+	} else {
+  }
+}
+
+function GlassModManagerBoardTCP::handleText(%this, %line) {
+	%this.buffer = %this.buffer NL %line;
+}
+
+
+
+
 function GlassModManagerTCP::onDone(%this, %error) {
-	echo("activity feed - " @ %this.buffer);
 	if(!%error) {
 		%array = parseJSON(%this.buffer);
 		if(getJSONType(%array) $= "array") {
@@ -149,13 +344,14 @@ function GlassModManagerTCP::onDone(%this, %error) {
         %date = %obj.get("datestring");
         GlassModManager.addActivityEvent(%id, %image, %text, %action, %date);
 			}
+      GlassModManager.renderActivity();
 		} else {
-      echo("not array");
+
     }
 	} else {
-    echo(%error);
+
   }
-  echo("all done here");
+
 }
 
 function GlassModManagerTCP::handleText(%this, %line) {
