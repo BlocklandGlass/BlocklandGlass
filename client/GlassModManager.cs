9373af702afa7133020ee8e777ccd8fbf8ac3564
Jincux <dephurites@gmail.com>
2015-07-28 11:29:26 -0400

    added default add-ons

--- client/GlassModManager.cs
+++ client/GlassModManager.cs
@@ -804,7 +804,80 @@ function GlassModManagerBoardTCP::handleText(%this, %line) {
 //====================================
 
 function GlassModManager_MyAddons::defaults() {
-  //uh...
+  echo("loading defaults");
+  for(%i = 0; %i < GlassModManagerGui_MyAddons.getCount(); %i++) {
+    %guiObj = GlassModManagerGui_MyAddons.getObject(%i);
+    %check = %guiObj.getObject(1);
+    $AddOn["__" @ %check.addon] = false;
+  }
+
+  $AddOn__Bot_Blockhead = 1;
+  $AddOn__Bot_Hole = 1;
+  $AddOn__Bot_Horse = 1;
+  $AddOn__Bot_Shark = 1;
+  $AddOn__Bot_Zombie = 1;
+  $AddOn__Brick_Arch = 1;
+  $AddOn__Brick_Checkpoint = 1;
+  $AddOn__Brick_Christmas_Tree = 1;
+  $AddOn__Brick_Doors = 1;
+  $AddOn__Brick_Halloween = 1;
+  $AddOn__Brick_Large_Cubes = 1;
+  $AddOn__Brick_Teledoor = 1;
+  $AddOn__Brick_Treasure_Chest = 1;
+  $AddOn__Brick_V15 = 1;
+  $AddOn__Emote_Alarm = 1;
+  $AddOn__Emote_Confusion = 1;
+  $AddOn__Emote_Hate = 1;
+  $AddOn__Emote_Love = 1;
+  $AddOn__Item_Key = 1;
+  $AddOn__Item_Skis = 1;
+  $AddOn__Item_Sports = 1;
+  $AddOn__Light_Animated = 1;
+  $AddOn__Light_Basic = 1;
+  $AddOn__Particle_Basic = 1;
+  $AddOn__Particle_FX_Cans = 1;
+  $AddOn__Particle_Grass = 1;
+  $AddOn__Particle_Player = 1;
+  $AddOn__Particle_Tools = 1;
+  $AddOn__Player_Fuel_Jet = 1;
+  $AddOn__Player_Jump_Jet = 1;
+  $AddOn__Player_Leap_Jet = 1;
+  $AddOn__Player_No_Jet = 1;
+  $AddOn__Player_Quake = 1;
+  $AddOn__Print_1x2f_BLPRemote = 1;
+  $AddOn__Print_1x2f_Default = 1;
+  $AddOn__Print_2x2f_Default = 1;
+  $AddOn__Print_2x2r_Default = 1;
+  $AddOn__Print_Letters_Default = 1;
+  $AddOn__Projectile_GravityRocket = 1;
+  $AddOn__Projectile_Pinball = 1;
+  $AddOn__Projectile_Pong = 1;
+  $AddOn__Projectile_Radio_Wave = 1;
+  $AddOn__Sound_Beeps = 1;
+  $AddOn__Sound_Phone = 1;
+  $AddOn__Sound_Synth4 = 1;
+  $AddOn__Support_Doors = 1;
+  $AddOn__Vehicle_Ball = 1;
+  $AddOn__Vehicle_Flying_Wheeled_Jeep = 1;
+  $AddOn__Vehicle_Horse = 1;
+  $AddOn__Vehicle_Jeep = 1;
+  $AddOn__Vehicle_Magic_Carpet = 1;
+  $AddOn__Vehicle_Pirate_Cannon = 1;
+  $AddOn__Vehicle_Rowboat = 1;
+  $AddOn__Vehicle_Tank = 1;
+  $AddOn__Weapon_Bow = 1;
+  $AddOn__Weapon_Gun = 1;
+  $AddOn__Weapon_Guns_Akimbo = 1;
+  $AddOn__Weapon_Horse_Ray = 1;
+  $AddOn__Weapon_Push_Broom = 1;
+  $AddOn__Weapon_Rocket_Launcher = 1;
+  $AddOn__Weapon_Spear = 1;
+  $AddOn__Weapon_Sword = 1;
+
+  $AddOn__System_BlocklandGlass = 1;
+
+  GlassModManager.renderMyAddons();
+  export("$AddOn__*", "config/server/ADD_ON_LIST.cs");
 }
 
 function GlassModManager_MyAddons::enableAll() {
@@ -813,6 +886,8 @@ function GlassModManager_MyAddons::enableAll() {
     %check = %guiObj.getObject(1);
     %check.setValue(true);
   }
+
+  $AddOn__System_BlocklandGlass = 1;
 }
 
 function GlassModManager_MyAddons::disableAll() {
@@ -821,6 +896,8 @@ function GlassModManager_MyAddons::disableAll() {
     %check = %guiObj.getObject(1);
     %check.setValue(false);
   }
+
+  $AddOn__System_BlocklandGlass = 1;
 }
 
 function GlassModManager_MyAddons::apply() {
@@ -832,11 +909,11 @@ function GlassModManager_MyAddons::apply() {
   }
 
   GlassModManager.renderMyAddons();
+  $AddOn__System_BlocklandGlass = 1;
   export("$AddOn__*", "config/server/ADD_ON_LIST.cs");
 }
 
 function GlassModManager::populateMyAddons(%this) {
-  GlassModManager::setLoading(false);
   discoverFile("Add-Ons/*.zip");
   if(isObject(GlassModManager_MyAddons)) {
     GlassModManager_MyAddons.delete();
@@ -853,6 +930,11 @@ function GlassModManager::populateMyAddons(%this) {
     if(strpos(%name, "/") >= 0) { //removes sub-directories
       continue;
     }
+
+    if(%name $= "System_BlocklandGlass") {
+      continue;
+    }
+    
     %so = new ScriptObject() {
       class = "GlassModManager_MyAddon";
       name = %name;
