a12712a422931ff47ad1ed0a1c37bbb6ba40282f
Devon Endicott <dephurites@gmail.com>
2015-11-13 10:49:59 -0500

    updated feedback for alpha 3

--- feedback.cs
+++ feedback.cs
@@ -1,13 +1,9 @@
 function Glass::openFeedbackPrompt() {
-  if(isfile("config/client/BLG/feedback.cs"))
-    exec("config/client/BLG/feedback.cs");
-
-  if($Glass::Feedback < 1) {
-    messageBoxOk("Feedback", "<font:quicksand-bold:24>Alpha Feedback<br><br><font:quicksand:16>Hey there. You're on the development branch of Blockland Glass, and we've officially entered our Alpha phase.<br><br>Currently, we're focusing testing on the new preferences system. If you didn't know, there's a full-fledge RTB-pref replacement available that should already have ALL your old RTB prefs on it!<br><br>Please, report any bugs you find with the system over in the <a:http://forum.blockland.us/index.php?topic=282486.0>development topic</a>. Active testing will help make Glass better for everyone.<br><br>Thanks,<br>Jincux", "Glass::feedbackSeen(1);");
+  if(GlassSettings.cacheFetch("Feedback") !$= "alpha3") {
+    messageBoxOk("Feedback", "<font:quicksand-bold:24>Alpha 3<br><br><font:quicksand:16>Welcome to Glass Alpha 3!<br><br>New in this build is a whole new (backend) setting and cache system. This has led to some changes to the mod manager, specifically the keybind and colorset system.<br><br>Please focus your testing on these, and let us know if you find anything. Also, if your previous colorset and keybind settings haven't transfered over, please notify us of that.<br><br>Thanks,<br>The Glass Team", "Glass::feedbackSeen(1);");
   }
 }
 
 function Glass::feedbackSeen(%iter) {
-  $Glass::Feedback = %iter;
-  export("$Glass::Feedback", "config/client/BLG/feedback.cs");
+  GlassSettings.cachePut("Feedback", "alpha3");
 }
