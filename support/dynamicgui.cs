d83943a9d9c716fe08c2d0e2273fed370870834d
Devon Endicott <dephurites@gmail.com>
2015-12-20 11:43:15 -0500

    dynamic gui

---
+++ support/dynamicgui.cs
@@ -0,0 +1,103 @@
+//================================================
+// Dynamic Gui Extension
+//
+// Jincux (9789)
+//
+// Just some functions to make the dynamic creation
+// of GUI elements a little easier
+//
+//================================================
+
+if($DynamicGui::Version > 1)
+  return;
+  
+$DynamicGui::Version = 1;
+
+// .setMargin (horizontal/[left], vertical/[right], [top], [bottom])
+function GuiObjectCtrl::setMargin(%this, %a, %b, %c, %d) {
+  if(%c !$= "" && %d !$= "") {
+    %left = %a;
+    %right = %b;
+    %top = %c;
+    %bottom = %d;
+  } else {
+    %left = %a;
+    %right = %a;
+    %top = %b;
+    %bottom = %b;
+  }
+
+  %this.position = %top SPC %left;
+}
+
+//resizes the object to be maximum size within the margin
+function GuiObjectCtrl::setMarginResize(%this, %a, %b, %c, %d) {
+  if(%c !$= "" && %d !$= "") {
+    %left = %a;
+    %right = %b;
+    %top = %c;
+    %bottom = %d;
+  } else {
+    %left = %a;
+    %right = %a;
+    %top = %b;
+    %bottom = %b;
+  }
+
+  %parent = %this.getGroup();
+
+  if(%a !$= "") { //only resize if we have a margin
+    %x = getWord(%parent.extent, 0)-%left-%right;
+  } else {
+    %x = getWord(%this.extent, 0);
+  }
+
+  if(%b !$= "") { //only resize if we have a margin
+    %y = getWord(%parent.extent, 0)-%top-%bottom;
+  } else {
+    %y = getWord(%this.extent, 0);
+  }
+
+  %this.position = %top SPC %left;
+  %this.extent = %x SPC %y;
+}
+
+//resizes the parent to be the size of the object+margins
+function GuiObjectCtrl::setMarginResizeParent(%this, %a, %b, %c, %d) {
+  if(%c !$= "" && %d !$= "") {
+    %left = %a;
+    %right = %b;
+    %top = %c;
+    %bottom = %d;
+  } else {
+    %left = %a;
+    %right = %a;
+    %top = %b;
+    %bottom = %b;
+  }
+
+  if(%a !$= "") { //only resize the parent if we have a margin
+    %x = getWord(%this.extent, 0)+%left+%right;
+  } else {
+    %x = getWord(%this.getGroup().extent, 0);
+  }
+
+  if(%b !$= "") { //only resize the parent if we have a margin
+    %y = getWord(%this.extent, 0)+%top+%bottom;
+  } else {
+    %y = getWord(%this.getGroup().extent, 0);
+  }
+
+  %this.position = %top SPC %left;
+  %this.getGroup().extent = %x SPC %y;
+
+}
+
+function GuiObjectCtrl::forceCenter(%this) {
+  %parent = %this.getGroup();
+
+  %x = mFloor((getWord(%parent.extent, 0)-getWord(%this.extent, 0))/2);
+  %y = mFloor((getWord(%parent.extent, 1)-getWord(%this.extent, 1))/2);
+
+  %this.position = %x SPC %y;
+}
