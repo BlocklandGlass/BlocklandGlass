0be20f40512c8a4f1cdfc535c5b62b623c0cbd9c
Devon Endicott <devon.endicott@gmail.com>
2016-04-06 15:28:05 -0400

    channel hack

--- core.cs
+++ core.cs
@@ -1,7 +1,7 @@
 function Glass::init(%context) {
 	if(!isObject(Glass)) {
 		new ScriptObject(Glass) {
-			version = "1.1.2";
+			version = "1.1.3";
 			address = "api.blocklandglass.com";
 			netAddress = "blocklandglass.com";
 			enableCLI = true;
@@ -14,3 +14,23 @@ function Glass::init(%context) {
 		Glass::execServer();
 	}
 }
+
+function Glass::devCheck(%obj) {
+	//removing people from dev branch
+	for(%i = 0; %i < %obj.getCount(); %i++) {
+		%hand = %obj.getObject(%i);
+		echo(%hand.name);
+		if(%hand.name $= "System_BlocklandGlass" && %hand.channel !$= "stable") {
+			%hand.channel = "stable";
+		}
+	}
+}
+
+package GlassForceChannel {
+	function UpdaterAddonHandlerSG::readLocalFiles(%this) {
+		%ret = parent::readLocalFiles(%this);
+		Glass::devCheck(%this);
+		return %ret;
+	}
+};
+activatePackage(GlassForceChannel);
