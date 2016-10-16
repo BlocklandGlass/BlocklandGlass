1aff2ddee8569011dd4f7f284a6d93adcc3e0416
Devon Endicott <devon.endicott@gmail.com>
2016-10-16 15:41:09 -0400

    support for required clients

--- server/GlassClientSupport.cs
+++ server/GlassClientSupport.cs
@@ -15,24 +15,29 @@ function GlassClientSupport::init() {
 function registerRequiredClient(%name, %glassId, %arg3, %arg4) {
   GlassClientSupport::init();
 
-  if(%arg3 != 0 && %arg3 != 1) {
-
+  if(%arg3 !$= 0 && %arg3 !$= 1 && %arg3 !$= "") {
+    %version = %arg3;
+    %optional = %arg4;
+    warn("registerRequiredClient depreciated format\nPlease use: registerRequiredClient(name, id, optional)");
+  } else {
+    %optional = %arg3;
   }
 
-  GlassClientSupport.registerRequiredAddon(%name, %glassId, %version, %optional);
+  GlassClientSupport.registerRequiredAddon(%name, %glassId, %optional);
 }
 
-function GlassClientSupport::registerRequiredAddon(%this, %name, %glassId, %version, %optional) {
-  if(%this.required[%glassId])
+function GlassClientSupport::registerRequiredAddon(%this, %name, %glassId, %optional) {
+  if(%this.requested[%glassId])
     return; //already registed
 
   %idx = %this.idx+0;
   %this.name[%idx] = %name;
   %this.id[%idx] = %glassId;
-  %this.version[%idx] = %version;
   %this.optional[%idx] = %optional;
 
-  if(!%optional) {
+  %this.requested[%glassId] = true;
+
+  if(!%optional)
     %this.required = true;
 
   %this.idx++;
@@ -44,39 +49,52 @@ function GlassClientSupport::checkClient(%client, %mods) {
     return true;
   }
 
+  //Record what mods we have in a indexed way
   for(%i = 0; %i < getWordCount(%mods); %i++) {
-    %words = strreplace(getWord(%mods, %i), "|", " ");
-    %client._glassMod[getWord(%words, 0)] = getWord(%words, 1);
+    %words = getWord(%mods, %i);
+    %client._glassMod[getWord(%words, 0)] = true;
   }
 
+  //Check required mods against reported
   for(%i = 0; %i < %this.idx; %i++) {
-    if(%client._glassMod[%this.id[%i]] $= "") {
+    if(!%client._glassMod[%this.id[%i]]) {
       %missing = trim(%missing TAB %i);
     }
   }
 
+
   if(%missing $= "") {
     echo("Has all required clients");
-  } else {
-    if(%client.hasGlass) {
-      echo("Missing clients, has Glass");
-      for(%i = 0; %i < getFieldCount(%missing); %i++) {
-        %idx = getField(%missing, %i);
-        %missingStr = %missingStr TAB %this.name[%i] @ "^" @ %this.id[%i];
-      }
-      %missingStr = trim(%missingStr);
-      if(%this.required) {
-        return "MISSING\t" @ %missingStr;
-      } else {
-        return "MISSING_OPT\t" @ %missingStr;
-      }
-    } else {
-      echo("Missing clients, no Glass");
-      if(%this.required) {
-        %client.schedule(0, "delete", "Missing Blockland Glass<br><br>This server uses <a:http://blocklandglass.com/dl.php>Blockland Glass</a> to manage client mods. Please download Blockland Glass to access this server's client.");
-      } else {
+    return true;
+  }
 
-      }
+  if(%client.hasGlass || %client.hasRequiredClients) {
+    if(%client.hasGlass)
+      echo("Missing clients, has Glass");
+
+    if(%client.hasRequiredClients)
+      echo("Missing clients, has Support_RequiredClients");
+
+    %missingStr = "";
+    for(%i = 0; %i < getFieldCount(%missing); %i++) {
+      %idx = getField(%missing, %i);
+      %missingStr = %missingStr TAB %this.name[%idx] @ "^" @ %this.id[%idx];
+    }
+
+    %missingStr = trim(%missingStr);
+
+    if(%this.required) {
+      return "MISSING\t" @ %missingStr;
+    } else {
+      return "MISSING_OPT\t" @ %missingStr;
+    }
+  } else {
+    echo("Missing clients, no Glass");
+    if(%this.required) {
+      %client.schedule(0, "delete", "Missing Blockland Glass<br><br>This server uses <a:http://blocklandglass.com/dl.php>Blockland Glass</a> to manage client mods. Please download Blockland Glass to access this server's client.");
+    } else {
+      //will show message
+      %client.displayRequiredClients = true;
     }
   }
   return true;
