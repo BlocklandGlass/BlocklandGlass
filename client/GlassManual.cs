4bd46e9ded8e7b02e33139300f9d81adb72a0de7
Devon Endicott <devon.endicott@gmail.com>
2016-12-11 20:27:17 -0500

    manual work

--- client/GlassManual.cs
+++ client/GlassManual.cs
@@ -1,7 +1,13 @@
 function GlassManual::init() {
   if(isObject(GlassManualWindow)) {
     GlassManualWindow.scan();
+  }
+}
+
+function GlassManualWindow::onWake(%this) {
+  if(!%this.firstWake) {
     GlassManualWindow.read("1. Credits");
+    %this.firstWake = true;
   }
 }
 
@@ -9,28 +15,34 @@ function GlassManualWindow::scan(%this) {
   GlassManualGui_Text.clear();
   GlassManualGui_List.clear();
 
-  %pattern = "Add-Ons/System_BlocklandGlass/resources/docs/*";
+  %pattern = "Add-Ons/System_BlocklandGlass/docs/*";
   %id = -1;
 	while((%file $= "" ? (%file = findFirstFile(%pattern)) : (%file = findNextFile(%pattern))) !$= "") {
-    %text = strreplace(%file, "Add-Ons/System_BlocklandGlass/resources/docs/", "");
+    %text = strreplace(%file, "Add-Ons/System_BlocklandGlass/docs/", "");
 
     %doc = getsubstr(%text, 0, strlen(%text) - 4);
 
     GlassManualGui_List.addRow(%id++, %doc);
   }
-  
+
   GlassManualGui_List.sortNumerical(0, true);
 }
 
 function GlassManualWindow::read(%this, %doc) {
-  GlassManualGui_Text.setText("<just:center>\n");
+  GlassManualGui_Text.setText("<font:verdana:12><color:333333>");
 
   %fo = new FileObject();
-  %fo.openForRead("Add-Ons/System_BlocklandGlass/resources/docs/" @ %doc @ ".txt");
+  %fo.openForRead("Add-Ons/System_BlocklandGlass/docs/" @ %doc @ ".txt");
   while(!%fo.isEOF())
-    GlassManualGui_Text.addText(%fo.readLine() @ "\n", true);
+    GlassManualGui_Text.addText(collapseEscape(%fo.readLine()) @ "\n", true);
   %fo.close();
   %fo.delete();
+
+  GlassManualGui_Text.setText(trim(GlassManualGui_Text.getText()));
+
+  GlassManualGui_Text.forceReflow();
+  GlassManualGui_Container.verticalMatchChildren(20, 10);
+  GlassManualGui_Container.setVisible(true);
 }
 
 function GlassManualGui_List::onSelect(%this, %rowID, %rowText) {
