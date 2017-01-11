1cb97c5a2e0d2bca7fea283b9c1fcd826aba9fd9
Jincux <devon.endicott@gmail.com>
2017-01-10 19:55:13 -0500

    v4 open beta 1

--- core.cs
+++ core.cs
@@ -1,7 +1,7 @@
 function Glass::init(%context) {
 	if(!isObject(Glass)) {
 		new ScriptObject(Glass) {
-			version = "4.0.0-alpha+indev";
+			version = "4.0.0-beta.1";
 			address = "api.blocklandglass.com"; //api address
 			netAddress = "blocklandglass.com"; //url address
 			enableCLI = true;
