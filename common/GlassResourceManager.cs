bf86f3b8e40bac97d02922af83790e001da0498d
Jincux <devon.endicott@gmail.com>
2017-01-13 23:17:23 -0500

    v4.0.0

--- common/GlassResourceManager.cs
+++ common/GlassResourceManager.cs
@@ -43,7 +43,7 @@ package GlassResourceManager {
     parent::onAdd(%this);
 
     %version["Support_Updater"] = "0.12.1+release-20160619";
-    %version["Support_Preferences"] = "1.2.0";
+    %version["Support_Preferences"] = "1.2.1";
     %channel["Support_Updater"] = "release";
     %channel["Support_Preferences"] = "stable";
     %repourl["Support_Updater"] = "mods.greek2me.us";
--- core.cs
+++ core.cs
@@ -1,7 +1,7 @@
 function Glass::init(%context) {
 	if(!isObject(Glass)) {
 		new ScriptObject(Glass) {
-			version = "4.0.0-beta.1+indev";
+			version = "4.0.0";
 			address = "api.blocklandglass.com"; //api address
 			netAddress = "blocklandglass.com"; //url address
 			enableCLI = true;
