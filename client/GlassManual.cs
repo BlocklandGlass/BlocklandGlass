f47885f9be996de8445fbba6ad9b92a2d63d5ed2
Jincux <devon.endicott@gmail.com>
2017-07-01 18:26:16 -0400

    added online manual sync #193

--- client/GlassManual.cs
+++ client/GlassManual.cs
@@ -11,6 +11,16 @@ function GlassManualWindow::onWake(%this) {
   }
 }
 
+function GlassManualWindow::downloadDocListing(%this) {
+  %url = "http://" @ Glass.address @ "/api/3/docs.php";
+  %method = "GET";
+  %downloadPath = "";
+  %className = "GlassManualTCP";
+
+  %tcp = connectToURL(%url, %method, %downloadPath, %className);
+  %tcp.requestType = "list";
+}
+
 function GlassManualWindow::scan(%this) {
   GlassManualGui_Text.clear();
   GlassManualGui_List.clear();
@@ -34,9 +44,30 @@ function GlassManualWindow::scan(%this) {
   }
 
   GlassManualGui_List.sortNumerical(0, true);
+  %this.downloadDocListing();
+}
+
+function GlassManualWindow::downloadDoc(%this, %doc) {
+  //echo("Downloading " @ %doc);
+
+  %url = "http://" @ Glass.address @ "/api/3/docs.php?doc=" @ urlEnc(%doc);
+  %method = "GET";
+  %downloadPath = "";
+  %className = "GlassManualTCP";
+
+  %tcp = connectToURL(%url, %method, %downloadPath, %className);
+  %tcp.requestType = "doc";
+  %tcp.doc  = %doc;
+
+  %this.loadingDoc = %doc;
 }
 
 function GlassManualWindow::read(%this, %doc) {
+  if(GlassManualWindow.useRemote) {
+    %this.downloadDoc(%doc);
+    return;
+  }
+
   GlassManualGui_Text.setText("<font:verdana:12><color:333333>");
 
   %fo = new FileObject();
@@ -55,5 +86,60 @@ function GlassManualWindow::read(%this, %doc) {
 
 function GlassManualGui_List::onSelect(%this, %rowID, %rowText) {
   %doc = trim(%rowText);
+
   GlassManualWindow.read(%doc);
+}
+
+function GlassManualTCP::handleText(%this, %line) {
+  if(%this.buffer $= "")
+    %this.buffer = %line;
+  else
+    %this.buffer = %this.buffer NL %line;
+}
+
+function GlassManualTCP::onDone(%this, %error) {
+  if(%error)
+    return; //will default to local
+
+  if(%this.requestType $= "list") {
+    GlassManualWindow.useRemote = true;
+    GlassManualGui_List.clear();
+
+    for(%j = 0; %j < getLineCount(%this.buffer); %j++) {%text = strreplace(%file, "Add-Ons/System_BlocklandGlass/docs/", "");
+      %text = getLine(%this.buffer, %j);
+
+      if(strlen(trim(%text)) == 0)
+        continue;
+
+      %doc = getsubstr(%text, 0, strlen(%text) - 4);
+      %indents = "";
+
+      for(%i = 0; %i < strlen(%doc); %i++) {
+        %letter = getsubstr(%doc, %i, 1);
+        if(%letter $= ".") {
+          %indents = %indents @ "  ";
+        }
+      }
+
+      GlassManualGui_List.addRow(%id++, %indents @ %doc);
+    }
+
+    GlassManualGui_List.sortNumerical(0, true);
+  } else {
+    if(GlassManualWindow.loadingDoc !$= %this.doc) {
+      //echo("Got late doc");
+      return;
+    }
+
+    //echo(%this.buffer);
+
+    GlassManualGui_Text.setText("<font:verdana:12><color:333333>");
+
+    GlassManualGui_Text.addText(collapseEscape(%this.buffer) @ "\n", true);
+    GlassManualGui_Text.setText(trim(GlassManualGui_Text.getText()));
+
+    GlassManualGui_Text.forceReflow();
+    GlassManualGui_Container.verticalMatchChildren(20, 10);
+    GlassManualGui_Container.setVisible(true);
+  }
 }
